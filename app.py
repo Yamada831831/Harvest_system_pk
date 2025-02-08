c85064fb74c063a8b946c410698ca016900067ff
Yamada831831 <yamada@harvest831.com>
2025-02-08 12:24:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Render!'
+    return "<h1>包材在庫管理システム</h1><p>ここに在庫データを表示</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
